678d2285e228c0d4d705e934e2187ac07cbd4466
Yahor Shybeka <egor.shibeko@gmail.com>
2023-07-27 11:24:10 -0400

    TEX Shura full matrix and ANSI layout

---
+++ keyboards/tex/shura/boards/TEX_SHURA/board.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define HT32F52352
+
+#if !defined(_FROM_ASM_)
+#ifdef __cplusplus
+extern "C" {
+#endif
+void boardInit(void);
+#ifdef __cplusplus
+}
+#endif
+#endif /* _FROM_ASM_ */
