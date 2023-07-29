7009e8c5b6fc6336db6ef1b8f155597160efb788
Yahor Shybeka <egor.shibeko@gmail.com>
2023-07-29 19:44:49 -0400

    TEX Shura trackpoint support

---
+++ keyboards/tex/shura/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "pin_defs.h"
+
+#define PS2_CLOCK_PIN B3
+#define PS2_DATA_PIN  B4
---
+++ keyboards/tex/shura/halconf.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef PS2_DRIVER_INTERRUPT
+#define PAL_USE_CALLBACKS TRUE
+#endif
+
+#include_next <halconf.h>
